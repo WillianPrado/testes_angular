d079bf242d2b469595012be4d6b9658452aa7b0c
WillianPrado <willian95prado@gmail.com>
2024-10-10 22:50:57 -0300

    Deploy

---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,5 @@
+export const environment = {
+    production: true,
+    apiUrl: 'https://primorossi.directlead.com.br',
+    otherVariable: 'valor de produção'
+  };
---
+++ src/environments/environment.ts
@@ -0,0 +1,4 @@
+export const environment = {
+    production: false,
+    apiUrl: 'https://primorossi.directlead.com.br'
+  };
